dd48637f6311c87309007c49f3fed8f8eb00554a
Muhammad Yousuf <muhammadyousuf327@gmail.com>
2021-11-05 10:23:48 -0700

    server setup

---
+++ server.js
@@ -0,0 +1,34 @@
+require('dotenv').config({ path: `./src/environment/${process.env.NODE_ENV}.env` })
+require("./src/config/db");
+
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const routes = require("./src/routes")
+
+const port = process.env.PORT;
+
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
+
+app.use(express.json());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Custom-header");
+    res.header("Access-Control-Expose-Headers", "X-Custom-header");
+    res.header("Access-Control-Expose-Headers", "authorization");
+    next();
+});
+
+app.use(cors());
+app.use("/api/user", routes.USER);
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
